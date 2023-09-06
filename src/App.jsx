3e41a23fb0b44fa45153ea4876f37fb7e986b9af
Leo <leo020120@outlook.com>
2023-09-06 20:50:41 +0100

    updated app.jsx and app.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import DataTable from './datatable';
+import './App.css'
 
 function App() {
   const [games, setGames] = useState([]);
@@ -25,8 +26,43 @@ function App() {
 
   return (
     <>
-      <h1>LMS</h1>
-      <DataTable data={games} /> 
+    <body>
+      <div className='container'>
+      <header>
+        
+        <nav className='navBar'>
+        <img className='logo' height='40px' width='40px' src='https://speckyboy.com/wp-content/uploads/2021/09/simple-logo-design-03.jpg' alt='logo'></img>
+          <ul>
+            <li className='item'><a href='#'>About</a></li>
+            <li className='item'><a href='#'>Help</a></li>
+          </ul>
+          <a className='login' href='#'><button>Login</button></a>
+        </nav>
+        
+      </header>
+      
+      <div className='main'>
+        <nav className='leftPanel'>
+          <ul>
+          <li>Premier League</li>
+          <li>Championship</li>
+          <li>League 1</li>
+          <li>League 2</li>
+          </ul>
+        </nav>
+        
+        <div className='table'>table
+        
+        
+        </div>
+        
+        <div className='rightPanel'>right panel</div>
+      </div>
+
+
+      <div className='footer'>footer</div>
+      </div>
+      </body>
     </>
   );
 }
